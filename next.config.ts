8ee6d5e4f8b29dad4bba64391cf73d2a5338b7e0
incehtc2 <ince.htce@gmail.com>
2026-04-16 07:48:52 +0300

    düzelme2

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true, // Vercel limitini aşmamak için eklendi
     formats: ["image/avif", "image/webp"],
     minimumCacheTTL: 3600,
     deviceSizes: [640, 750, 828, 1080, 1200],
